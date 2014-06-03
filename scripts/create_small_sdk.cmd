9e630f4722b96b5ba2ac25ce99ac278f53fe5f37
mavl <mavl@tmapy.cz>
2014-06-03 14:41:41 +0200

    script to copy sdk without folders test, demo, tutorials...

---
+++ scripts/create_small_sdk.cmd
@@ -0,0 +1,6 @@
+cd..
+cd..
+XCOPY WebViewer\external\* wv\external\ /s /i
+XCOPY WebViewer\source\core\* wv\source\core /s /i
+XCOPY WebViewer\source\externs\* wv\source\externs /s /i
+XCOPY WebViewer\source\shaders\* wv\source\shaders /s /i
