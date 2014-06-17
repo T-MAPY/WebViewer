c2819a1011f9b059b1525cbe95f65440583e7dfc
mavl <mavl@tmapy.cz>
2014-06-17 11:29:42 +0200

    create_small_sdk script fix(copy compiled/deps.js)

--- scripts/create_small_sdk.cmd
+++ scripts/create_small_sdk.cmd
@@ -1,6 +1,7 @@
 cd..
 cd..
 XCOPY WebViewer\external\* wv\external\ /s /i
+XCOPY WebViewer\compiled\deps.js wv\compiled\deps.js
 XCOPY WebViewer\source\core\* wv\source\core /s /i
 XCOPY WebViewer\source\externs\* wv\source\externs /s /i
 XCOPY WebViewer\source\shaders\* wv\source\shaders /s /i
